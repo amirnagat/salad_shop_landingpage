57e85a7f49194ab6efd3984a3fb6de48c36d4267
amir <nagat1700@gmail.com>
2023-05-09 13:31:57 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Home from './Components/Home'
+import Testimonial from './Components/Testimonial'
+import Work from './Components/Work'
+import Footer from './Components/Footer'
+
+function App() {
+
+  return (
+   
+   <div className="App">
+      <Home />
+      <About />
+      <Work />
+      <Testimonial />
+      <Contact />
+      <Footer />
+    </div>
+  
+  )
+}
+
+export default App
