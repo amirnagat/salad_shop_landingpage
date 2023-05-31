1b6ae463b50fd47bd314fa74983565417a13fc27
amir <nagat1700@gmail.com>
2023-05-31 23:45:19 +0300

    commit

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -26,9 +26,9 @@ const Work = () => {
       <div className="work-section-top">
         <p className="primary-subheading">Work</p>  
         <h1 className="primary-heading">How It Works?</h1>   
-        <p className="primary-text"> 
-          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et
-          elit. Dolor turpis molestie dui magnis facilisis at fringilla quam.
+        <p className="primary-text">  
+          Lorem ipsum dolor sit amet consectetur. Non tincidunt magna non et 
+          elit. Dolor turpis molestie dui magnis facilisis at fringilla quamasd.
         </p>
       </div>
       <div className="work-section-bottom">
